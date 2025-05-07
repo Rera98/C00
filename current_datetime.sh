b086821c54e45bef60c0cd174e1bcc63981b4169
foufou <foufou@MacBook-Pro-de-fourera.local>
2025-05-07 11:10:35 +0100

    second

---
+++ current_datetime.sh
@@ -0,0 +1,2 @@
+#!/bin/bash  #ce code spécifie l'interpreteur qui doit être utilisé pour exécuter le script
+echo $(date '+%Y-%m-%d %H:%M:%S') #date récupère la date selon le format indiqué et l'affiche
