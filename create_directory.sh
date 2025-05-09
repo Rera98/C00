069d893a2c3a67ae00d7b0187ffaedc99146acae
foufou <foufou@MacBook-Pro-de-fourera.local>
2025-05-09 08:35:14 +0100

    commit

--- create_directory.sh
+++ create_directory.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 # Nom du répertoire à créer
-repertoire="test_directory"
+dossier="test_directory"
 
-# Vérifie si le répertoire existe déjà
-if [ -d "$repertoire" ]; then
-    echo "Le répertoire '$repertoire' existe déjà."
-else
-    mkdir "$repertoire"
-    echo "Le répertoire '$repertoire' a été créé."
-fi
+# Créer le répertoire (sans erreur s'il existe déjà)
+mkdir -p "$dossier"
+
+# Message de confirmation
+echo "Le répertoire '$dossier' a été créé."
