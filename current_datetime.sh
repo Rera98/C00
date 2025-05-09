b145183abeea12d58c9318a2a9f9afb7d2cc1a19
foufou <foufou@MacBook-Pro-de-fourera.local>
2025-05-09 11:11:59 +0100

    commit

--- current_datetime.sh
+++ current_datetime.sh
@@ -1,3 +1,3 @@
 #!/bin/bash  
 #ce code spécifie l'interpreteur qui doit être utilisé pour exécuter le script
-date "+%Y-%m-%d %HH:%MM:%SS" #date récupère la date selon le format indiqué et l'affiche
+date "+%Y-%m-%d %H:%M:%S" #date récupère la date selon le format indiqué et l'affiche
