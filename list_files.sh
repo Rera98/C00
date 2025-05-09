395e3f4a9e34b30a2d223f9a88136558e3e913a6
foufou <foufou@MacBook-Pro-de-fourera.local>
2025-05-09 11:36:28 +0100

    commit

--- list_files.sh
+++ list_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Ce script affiche tous les fichiers (et dossiers) du répertoire courant, un par ligne
-for item in *; do
-  if [ -f "$item" ]; then
-    ls -l -- "$item"
+for item in *; do #parcourt tous les éléments visibles du répertoire courant.
+  if [ -f "$item" ]; then #filtre uniquement les fichiers réguliers.
+    ls -l -- "$item"  #affiche les infos du fichier, avec -- pour éviter les erreurs si un nom commence par -.
   fi
 done
